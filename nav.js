c7c6ff2f0b09393e1fbedb631270c01caea9520d
takareinhard <hokke.takara@gmail.com>
2020-01-13 22:23:14 +0900

    メニュー改正

--- nav.js
+++ nav.js
@@ -1,11 +1,19 @@
-$(".menu-trigger").on("click", function(){
-  $(this).toggleClass("active");
-  $(".navigation").toggleClass("fix");//追記
+$(".menu-trigger").on("click", function () {
+  const e = document.getElementById("navs");
+  if (e.style.display = "block") {
+    $(this).toggleClass("active");
+    $(".navigation").toggleClass("fix");//追記
+  } else {
+    e.style.display = "block";
+    $(".navigation").toggleClass("fix");//追記
+  }
 });
 
 document.getElementById("navs").onclick = function () {
   const e = document.getElementById("navs");
   e.style.display = "none";
+  $(".menu-trigger").toggleClass("active");
+  $(".navigation").removeClass('fix');
 };
 
 // document.getElementById("navs").onclick = function () {
